9b45771b97978e1e606273ab6775f3ca9c5a5d98
Kei <euldonga@gmail.com>
2019-02-28 16:58:31 +0900

    요구사항 1 해결.
    Signed-off-by: Kei <euldonga@gmail.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,13 +27,29 @@ public class RequestHandler extends Thread {
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			byte[] body = Files.readAllBytes(new File(getViewName(in)).toPath());
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+	
+	private String getViewName(InputStream in) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String header = null;
+		try {
+			while ((header = br.readLine()) != null) {
+				if (header.startsWith("GET")) {
+					String view = header.split("\\s")[1];
+					return "./webapp" + view;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -1,7 +1,9 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.StringReader;
+import java.nio.file.Files;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -17,5 +19,25 @@ public class IOUtilsTest {
 		BufferedReader br = new BufferedReader(sr);
 		
 		logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+	}
+	
+	@Test
+	public void someTest() throws Exception {
+		byte[] body = "Hello World".getBytes();
+		logger.debug("getBytes : {}", body);
+	}
+	
+	@Test
+	public void filePath() throws Exception {
+		logger.debug("path 1 : {}", new File("./webapp").getCanonicalPath());
+		logger.debug("path 2 : {}", new File(".").getCanonicalPath());
+		logger.debug("path 3 : {}", new File("../").getCanonicalPath());
+		logger.debug("path 4 : {}", new File("../../").getCanonicalPath());
+	}
+	
+	@Test
+	public void byteToChar() throws Exception {
+		byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+		logger.debug("bytes: {}", new String(body));
 	}
 }
